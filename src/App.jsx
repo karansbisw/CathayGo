8403880e1be7fb920ce8a0fbb7b35a24a5ed4df7
Aditya <aditya@Adityas-MacBook-Air.local>
2024-11-06 19:15:37 +0800

    tailwind int

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,52 @@
 import React, { useState, useEffect } from "react";
 import MapComponent from "./MapComponent";
 import ARMap from "./ARMap";
+import Popup from "./components/Popup";
 
 const App = () => {
-  const [userLocation, setUserLocation] = useState(null);
-  const [selectedLocation, setSelectedLocation] = useState(null);
+  return <>
+    <Popup />
+  </>
+  // const [userLocation, setUserLocation] = useState(null);
+  // const [selectedLocation, setSelectedLocation] = useState(null);
 
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setUserLocation({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
-        },
-        (error) => {
-          console.error("Error getting user location:", error);
-        }
-      );
-    } else {
-      console.error("Geolocation is not supported by this browser.");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(
+  //       (position) => {
+  //         setUserLocation({
+  //           lat: position.coords.latitude,
+  //           lng: position.coords.longitude,
+  //         });
+  //       },
+  //       (error) => {
+  //         console.error("Error getting user location:", error);
+  //       }
+  //     );
+  //   } else {
+  //     console.error("Geolocation is not supported by this browser.");
+  //   }
+  // }, []);
 
-  const handleSelectLocation = (location) => {
-    setSelectedLocation(location);
-  };
+  // const handleSelectLocation = (location) => {
+  //   setSelectedLocation(location);
+  // };
 
-  return (
-    <div>
-      <h1>Navigation Map</h1>
-      {selectedLocation ? (
-        <ARMap location={selectedLocation} />
-      ) : userLocation ? (
-        <MapComponent
-          userLocation={userLocation}
-          onSelectLocation={handleSelectLocation}
-        />
-      ) : (
-        <p>Loading user location...</p>
-      )}
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <h1>Navigation Map</h1>
+  //     {selectedLocation ? (
+  //       <ARMap location={selectedLocation} />
+  //     ) : userLocation ? (
+  //       <MapComponent
+  //         userLocation={userLocation}
+  //         onSelectLocation={handleSelectLocation}
+  //       />
+  //     ) : (
+  //       <p>Loading user location...</p>
+  //     )}
+  //   </div>
+  // );
 };
 
 export default App;
